22c501a7070e84a81f4b1dbb5722ed0078e08109
Adrian Ixta <1077056@uvu.edu>
2024-03-08 20:56:31 -0700

    Added and completed challenge 2
    Completed bonuses and requirements

---
+++ Prototype 2 Project/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnDelay = 0.8f;
+    private bool onDelay = false;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&&!onDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+           
+            onDelay = true;
+           
+            Invoke("delayFunction", spawnDelay);
+        }
+        
+    }
+   // reset delay to false (not entirely sure why I need this but I do since it does not work without it)
+    void delayFunction()
+    {
+        onDelay = false;
+    }
+}
+// If space bar and NOT onDelay (meaning is false) spawn dog set delay to true Invoke "delayFunction" After 0.8 sec have passed.
---
+++ Prototype 2 Project/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+    private float minTime = 3f;
+    private float maxTime = 5f;
+    private float spawnRandomTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+     // removed start call and moved it into Void Update.
+    }
+    void Update()// Invoking spawn method and giving it a random range (I think I could remove the if statement)
+    {
+        if (!IsInvoking("SpawnRandomBall"))
+        {
+            Invoke("SpawnRandomBall", Random.Range(minTime, maxTime));
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+       // Crated index random selection by starting at zero and selecting from the index length
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+}
